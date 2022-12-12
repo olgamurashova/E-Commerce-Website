3a66c83bf5c7ad113e30f7f3121ea8a031ce8c42
Olga Murashova <om.dreyfus@gmail.com>
2022-12-12 13:32:27 -0500

    cart & products style.css

--- js/index.js
+++ js/index.js
@@ -27,3 +27,27 @@ window.addEventListener('scroll', ()=> {
 
   }
 })
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map((link) => {
+  if (!link) return;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
